3d9fc8ab4cf1d69cbb045598ffeb9a9822ea23d6
Anish Kumar <anish.se@gmail.com>
2026-07-07 12:48:56 +0530

    oops completed

---
+++ src/main/scala/scala/udemy/learn/oops/AnonymousClass.scala
@@ -0,0 +1,22 @@
+package scala.udemy.learn.oops
+
+object AnonymousClass extends App {
+
+  abstract class Animal {
+    def eat(): Unit
+  }
+
+  /*
+    it is an extending syntax sugar for extending and instantiating a class.
+   */
+  val funnyAnimal = new Animal {
+    override def eat(): Unit = println("My funny Animal......")
+  }
+  println(funnyAnimal.getClass)
+
+  class AnonymousClass$$anon$1 extends Animal {
+    override def eat(): Unit = println("compiler's funny Animal............")
+  }
+  val compilerFunnyAnimal = new AnonymousClass$$anon$1
+
+}
